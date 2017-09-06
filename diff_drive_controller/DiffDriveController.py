93a3811f44868fa1877213eb5a9076289f1011e1
Andrew Dahdouh <andrew.dahdouh@gmail.com>
2017-09-06 11:15:46 -0500

    Updated and fixed controller. Tests written and tested on robot with AR tag.

--- diff_drive_controller/DiffDriveController.py
+++ diff_drive_controller/DiffDriveController.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# V1.2
 import numpy as np
 
 
@@ -8,9 +9,9 @@ class DiffDriveController():
     Class used for controlling the robot linear and angular velocity
     """
     def __init__(self, max_speed, max_omega):
-        self.kp=1  # Must be positive
-        self.ka=2  # Must be positive with ka - kp > 0
-        self.kb=-1 # Must be negative
+        self.kp = .5  # Must be positive
+        self.ka = 1  # Must be positive with ka - kp > 0
+        self.kb = -.5  # Must be negative
         self.error_tol = 0.05
         self.MAX_SPEED = max_speed
         self.MAX_OMEGA = max_omega
@@ -34,59 +35,69 @@ class DiffDriveController():
         done - a boolean value specifying if the robot has reached its goal (or
             is close enough
         """
-        # Calculate the magnitude of error:
 
-        delta_x = goal[0] - state[0]
-        delta_y = goal[1] - state[1]
+        KP = self.kp
+        KA = self.ka
+        KB = self.kb
+        delx = goal[0] - state[0]
+        dely = goal[1] - state[1]
+        theta = state[2]
+        tworoots = np.roots([1, KA - KP, -KP * KB])
+        w = [-KP, tworoots[0], tworoots[1]]
+        # if w[0]<0 and w[1]<0 and w[2]<0:
+        p = np.sqrt((delx ** 2) + (dely ** 2))
+        alpha = -theta + np.arctan2(dely, delx)
+        beta = -theta - alpha
 
-        rho = np.sqrt(np.square(delta_x) + np.square(delta_y))
-
-        alpha = -state[2] + np.arctan2(delta_y,delta_x)
-        # print "alpha ", alpha, "\n"
-
-        beta = - state[2] - alpha
-        # print "beta: ", beta, "\n"
-
-        # Check if state is close enough to goal. If not:
-        # Use calculated to develop an acceptable velocity and omega value.
-
-        gain_matrix  = np.matrix([[self.kp, 0, 0], [0, self.ka, self.kb]])
-        # print gain_matrix
-        # print "\n"
-
-        error_matrix = np.matrix([[rho], [alpha], [beta]])
-        # print error_matrix
-        # print "\n"
-
-        vel_omega = np.matmul(gain_matrix, error_matrix)
-        # print "lin and ang vel: ", vel_omega, "\n"
-
-        if vel_omega[0] > self.MAX_SPEED:
+        v = p * KP
+        if np.all(v > self.MAX_SPEED):
             v = self.MAX_SPEED
-        else:
-            v = vel_omega[0]
 
-        if vel_omega[1] > self.MAX_OMEGA:
+        omega = (alpha * KA) + (beta * KB)
+        if np.all(omega > self.MAX_OMEGA):
             omega = self.MAX_OMEGA
-        else:
-            omega = vel_omega[1]
 
-        if delta_x < self.error_tol and delta_y < self.error_tol:
+        if np.all(p < 0.15):
             done = True
         else:
             done = False
 
-        return v, omega, done
-
+        vw = (v, omega, done)
+        return vw
 
 
 if __name__ == '__main__':
+
+    diff_drive_controller = DiffDriveController(0.5, 1.6)
+
     # Tests:
-
-    test_start = np.array([0,0,0])
-    test_goal = np.array([5,5, np.pi/4]) # 5m, 5m, 45 degrees
-
-    diff_drive_controller = DiffDriveController(0.5, np.pi/3)  # 5 m/s, 360 Degrees
+    print "\nRunning test::Straight Forward: (5, 0, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([5, 0, 0])
     print diff_drive_controller.compute_vel(test_start, test_goal)
 
+    print "\nRunning test::Left: (5m, 5m, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([5, 5, 0])
+    print diff_drive_controller.compute_vel(test_start, test_goal)
 
+    print "\nRunning test::Right: (5m, -5m, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([5, -5, 0])  # 5m, 5m, 45 degrees
+    print diff_drive_controller.compute_vel(test_start, test_goal)
+
+    # return [dx,dy,self._angle,self._marker_num]
+    # ('Measurements: ', [0.25480768607482129, -0.022549956977450526, 0.016922701984059048, 0])
+    # ('Computed command vel: ', (0.25580355244306036, 1.6, False))
+    # state = np.array([meas[0], meas[1], meas[2]])
+    # vw = self.diff_drive_controller.compute_vel(state, goal)
+
+    print "\nRunning Test::Tag: (0.25480768607482129, -0.022549956977450526, 0.016922701984059048)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([0.25480768607482129, -0.022549956977450526, 0.016922701984059048])  # 5m, 5m, 45 degrees
+    print diff_drive_controller.compute_vel(test_start, test_goal)
+
+    print "\nRunning Test::Tag: (0.2329683769187918, 0.011103904204575101, -0.084052242784899228, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([0.2329683769187918, 0.011103904204575101, -0.084052242784899228, 0])  # 5m, 5m, 45 degrees
+    print diff_drive_controller.compute_vel(test_start, test_goal)
