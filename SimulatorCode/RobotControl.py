7d543a7e33adf8602657df3ec0a867c417dc2b74
Andrew Dahdouh <andrew.dahdouh@gmail.com>
2017-09-19 07:36:35 -0500

    Pushing catkin packages for robot hardware.

--- SimulatorCode/RobotControl.py
+++ SimulatorCode/RobotControl.py
@@ -18,8 +18,8 @@ import matplotlib.pyplot as plt
 # from ros_interface import ROSInterface
 
 # TODO for student: User files, uncomment as completed
-from path_planner import dijkstras
-#from KalmanFilter import KalmanFilter
+# from path_planner import dijkstras
+from KalmanFilter import KalmanFilter
 from DiffDriveController import DiffDriveController
 
 class RobotControl(object):
@@ -66,7 +66,7 @@ class RobotControl(object):
         # YOUR CODE AFTER THIS
         
         # Uncomment as completed
-        #self.kalman_filter = KalmanFilter(world_map)
+        self.kalman_filter = KalmanFilter(world_map)
         self.diff_drive_controller = DiffDriveController(max_speed, max_omega)
 
     def process_measurements(self):
@@ -78,9 +78,7 @@ class RobotControl(object):
         # TODO for student: Comment this when running on the robot 
         meas = self.robot_sim.get_measurements()
         imu_meas = self.robot_sim.get_imu()
-        print "IMU Data: ", imu_meas
-        print "IMU Type: ", type(imu_meas)
-        print "First element: ", imu_meas[1]
+        print "meas: ", meas
         if meas != None:
             goal = np.array([0.5, 1.0])
             state = np.array([meas[0], meas[1], meas[2]])
@@ -89,7 +87,7 @@ class RobotControl(object):
                 self.robot_sim.command_velocity(vw[0], vw[1])
             else:
                 self.robot_sim.command_velocity(0, 0)
-
+            print "Get GT Pose: ", self.robot_sim.get_gt_pose()
             # TODO for student: Use this when transferring code to robot
             # meas = self.ros_interface.get_measurements()
             # imu_meas = self.ros_interface.get_imu()
---
+++ robot_control/src/DiffDriveController.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+# V1.2
+import numpy as np
+
+
+class DiffDriveController():
+    """
+    Class used for controlling the robot linear and angular velocity
+    """
+    def __init__(self, max_speed, max_omega):
+        self.kp = .5  # Must be positive
+        self.ka = 1  # Must be positive with ka - kp > 0
+        self.kb = -.5  # Must be negative
+        self.error_tol = 0.05
+        self.MAX_SPEED = max_speed
+        self.MAX_OMEGA = max_omega
+
+        # Characteristic Polynomial: 
+        # (lambda + kp)(lambda^2 + (ka - kp)*lambda - kp*kb) = 0
+
+
+        
+    def compute_vel(self, state, goal):
+        """
+        Function that computes the desired outputs given the state and goal
+        Inputs:
+        state - a numpy vector of size 3 by 1 with components (x,y,theta)
+        goal - a numpy vector of size 2 by 1 specifying the location of the goal
+        Outputs: a tuple with 3 elements
+        v - a number specifying the forward speed (in m/s) of the robot (should 
+            be no more than max_speed)
+        omega - a number specifying the angular velocity (in rad/s) of the robot
+            (should be no more than max_omega)
+        done - a boolean value specifying if the robot has reached its goal (or
+            is close enough
+        """
+
+        KP = self.kp
+        KA = self.ka
+        KB = self.kb
+        delx = goal[0] - state[0]
+        dely = goal[1] - state[1]
+        theta = state[2]
+        tworoots = np.roots([1, KA - KP, -KP * KB])
+        w = [-KP, tworoots[0], tworoots[1]]
+        # if w[0]<0 and w[1]<0 and w[2]<0:
+        p = np.sqrt((delx ** 2) + (dely ** 2))
+        alpha = -theta + np.arctan2(dely, delx)
+        beta = -theta - alpha
+
+        v = p * KP
+        if np.all(v > self.MAX_SPEED):
+            v = self.MAX_SPEED
+
+        omega = (alpha * KA) + (beta * KB)
+        if np.all(omega > self.MAX_OMEGA):
+            omega = self.MAX_OMEGA
+
+        if np.all(p < 0.15):
+            done = True
+        else:
+            done = False
+
+        vw = (v, omega, done)
+        return vw
+
+
+if __name__ == '__main__':
+    diff_drive_controller = DiffDriveController(0.5, 1.6)
+
+    # Tests:
+    print "Running test::Straight Forward: (5, 0, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([5, 1, 0])  # 5m, 5m, 45 degrees
+    diff_drive_controller.compute_vel(test_start, test_goal)
+
+
+    print "Running test::Turning: (5m, 5m, 45 Degrees)"
+    test_start = np.array([0,0,0])
+    test_goal = np.array([5,5, np.pi/4]) # 5m, 5m, 45 degrees
+    diff_drive_controller.compute_vel(test_start, test_goal)
+
+    # return [dx,dy,self._angle,self._marker_num]
+    # ('Measurements: ', [0.25480768607482129, -0.022549956977450526, 0.016922701984059048, 0])
+    # ('Computed command vel: ', (0.25580355244306036, 1.6, False))
+    # state = np.array([meas[0], meas[1], meas[2]])
+    # vw = self.diff_drive_controller.compute_vel(state, goal)
+
+    print "Running Test::Tag: (0.25480768607482129, -0.022549956977450526, 0.016922701984059048)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([0.25480768607482129, -0.022549956977450526, 0.016922701984059048])  # 5m, 5m, 45 degrees
+    print diff_drive_controller.compute_vel(test_start, test_goal)
+
+    print "Running Test::Tag: (0.2329683769187918, 0.011103904204575101, -0.084052242784899228, 0)"
+    test_start = np.array([0, 0, 0])
+    test_goal = np.array([0.2329683769187918, 0.011103904204575101, -0.084052242784899228, 0])  # 5m, 5m, 45 degrees
+    print diff_drive_controller.compute_vel(test_start, test_goal)
